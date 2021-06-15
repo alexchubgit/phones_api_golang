bdaf9ea156e22874ac418a95b81c7b36358c94f2
Alex Chub <alex.chub.24@gmail.com>
2021-06-15 17:53:30 +0900

    work 15.06.2021

--- routes/persons/persons.go
+++ routes/persons/persons.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -663,11 +662,11 @@ func UpdatePerson(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
 	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	//fmt.Printf("MIME Header: %+v\n", handler.Header)
 
 	// Create a temporary file within our temp-images directory that follows
 	// a particular naming pattern
-	tempFile, err := ioutil.TempFile("./static/photo/", "upload-*.png")
+	tempFile, err := os.CreateTemp("./static/photo/", "upload-*.png")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -675,12 +674,22 @@ func UpdatePerson(w http.ResponseWriter, r *http.Request) {
 
 	// read all of the contents of our uploaded file into a
 	// byte array
-	fileBytes, err := ioutil.ReadAll(file)
+	fileBytes, err := io.ReadAll(file)
 	if err != nil {
 		fmt.Println(err)
 	}
 	// write this byte array to our temporary file
 	tempFile.Write(fileBytes)
+
+	// Copy the uploaded file to the created file on the filesystem
+	if _, err := io.Copy(tempFile, file); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//fmt.Println(tempFile.Name())
+	defer os.Remove(tempFile.Name()) // clean up
+
 	// return that we have successfully uploaded our file!
 	fmt.Fprintf(w, "Successfully Uploaded File\n")
 

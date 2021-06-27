1a4aed1efdaa38fa7d35fb31d56060648de57c09
Alex Chub <alex.chub.24@gmail.com>
2021-06-28 00:55:08 +0900

    auth 28.06.2021

--- routes/auth/auth.go
+++ routes/auth/auth.go
@@ -20,6 +20,18 @@ import (
 Структура прав доступа JWT
 */
 
+type Maker interface {
+	CreateToken(username string, duration time.Duration) (string, error)
+	VerifyToken(token string) (*Payload, error)
+}
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
 type Token struct {
 	AccessToken  string
 	RefreshToken string
@@ -27,8 +39,7 @@ type Token struct {
 	RefreshUuid  string
 	AtExpires    int64
 	RtExpires    int64
-
-	UserId uint
+	UserId       uint
 	jwt.StandardClaims
 }
 
@@ -48,6 +59,7 @@ type Auth struct {
 var db *sql.DB
 var err error
 
+//Генерация хэша на основе пароля
 func HashPassword(password string) (string, error) {
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -56,10 +68,13 @@ func HashPassword(password string) (string, error) {
 		return "", fmt.Errorf("failed to hash password: %w", err)
 	}
 
+	fmt.Println("hashedPassword: " + string(hashedPassword))
+
 	return string(hashedPassword), nil
 }
 
-func CheckPassword(password string, hashedPassword string) bool {
+//Проверка bcrypt хэша с помощью пароля
+func CheckPassword(hashedPassword string, password string) bool {
 
 	if (hashedPassword == "") || (password == "") {
 		fmt.Println("Hash & password empty")
@@ -81,13 +96,22 @@ func CheckPassword(password string, hashedPassword string) bool {
 
 }
 
+//Проверка JWT хэша в маршрутах
 func CheckSecurity(password string, next http.HandlerFunc) http.HandlerFunc {
 	return func(res http.ResponseWriter, req *http.Request) {
 		//fmt.Println("middleware")
 		//fmt.Println(password)
 
-		//token := req.Header.Get("Authorization")
-		//fmt.Println(token)
+		token := req.Header.Get("Authorization")
+		fmt.Println(token)
+
+		// Initialize a new instance of `Claims`
+
+		jwtToken, err := jwt.ParseWithClaims(token, &Token{}, []byte("secretKey"))
+		if err != nil {
+
+			return
+		}
 
 		// header := req.Header.Get("Super-Duper-Safe-Security")
 		// if header != "password" {
@@ -96,6 +120,32 @@ func CheckSecurity(password string, next http.HandlerFunc) http.HandlerFunc {
 		// 	return
 		// }
 		next(res, req)
+
+		//     //Проверка токена из видео урока
+		//     const authHeader = req.get('Authorization');
+
+		//     if (!authHeader) {
+		//         res.status(401).json({ success: false, message: "Token not provided!" });
+		//     }
+
+		//     const token = authHeader.replace('token ', '')
+
+		//     //console.log('middleware ' + token);
+
+		//     try {
+		//         const decoded = jwt.verify(token, SECRET_KEY);
+
+		//         //ошибка заголовков
+		//         //res.status(200).json({ success: true, message: 'Good to authenticate token.' });
+
+		//         //console.log('decode ' + decoded.role + ' ' + decoded.name);
+		//         res.send(decoded);
+
+		//     } catch (e) {
+		//         if (e instanceof jwt.JsonWebTokenError) {
+		//             res.status(401).json({ success: false, message: "Token invalid!" });
+		//         }
+		//     }
 	}
 }
 
@@ -137,7 +187,9 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	//Get hash from database
 
-	var hash string
+	var hashedPassword string
+
+	//Ищем человека по номеру телефона личному или служебному и вытаскиваем хэш brcypt
 
 	result, err := db.Query("SELECT hash FROM persons WHERE cellular = ?  OR business = ? LIMIT 1", login, login)
 
@@ -149,22 +201,15 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 	for result.Next() {
 
-		err := result.Scan(&hash)
+		err := result.Scan(&hashedPassword)
 
 		if err != nil {
 			panic(err.Error())
 		}
 	}
 
-	//Output hash from db and password
-	//fmt.Println(hash)
-	//fmt.Println(password)
-
-	//fmt.Println(HashPassword(password))
-
-	//Check brypt
-
-	if CheckPassword(password, hash) {
+	//Проверяем полученный из базы хэш с помощью введенного пароля
+	if CheckPassword(hashedPassword, password) {
 
 		fmt.Println("OK")
 
@@ -206,6 +251,20 @@ func Login(w http.ResponseWriter, r *http.Request) {
 		//refreshToken, _ := rt.SignedString([]byte(os.Getenv("refreshKey")))
 		//fmt.Println(refreshToken)
 
+		//payload = {
+		//                 idperson: results[0].idperson,
+		//                 name: results[0].name,
+		//                 phone: results[0].cellular,
+		//                 role: results[0].role
+		//             };
+
+		//             //console.log(payload)
+		//             console.log(SECRET_KEY)
+		//             //здесь создается JWT
+		//             const token = jwt.sign(payload, SECRET_KEY, {
+		//                 expiresIn: 60 * 60 * 24 // истекает через 24 часа
+		//             });
+
 	} else {
 
 		fmt.Println("Does not OK")
@@ -224,105 +283,5 @@ func Login(w http.ResponseWriter, r *http.Request) {
 
 }
 
-//'SELECT * FROM persons LEFT JOIN role USING(idrole) WHERE `cellular` = "' + login + '" AND `passwd` = "' + password + '" OR `business` = "' + login + '" AND `passwd` = "' + password + '" LIMIT 1'
-
-// //Авторизация
-// auth.post('/login', async (req, res) => {
-
-//     const login = req.body.login;
-//     const password = md5(req.body.password);
-//     let payload = {}
-
-//     //console.log(login)
-
-//     if ((login == undefined) && (password == undefined)) {
-//         return res.sendStatus(500);
-//     }
-
-//     try {
-//         const results = await Login(login, password);
-
-//         //console.log(results)
-
-//         //здесь проверяем авторизацию и создаем токен
-//         if (results.length == 0) {
-//             res.json({ success: false, message: 'Логин или пароль указаны неверно' });
-//         } else {
-//             payload = {
-//                 idperson: results[0].idperson,
-//                 name: results[0].name,
-//                 phone: results[0].cellular,
-//                 role: results[0].role
-//             };
-
-//             //console.log(payload)
-//             console.log(SECRET_KEY)
-//             //здесь создается JWT
-//             const token = jwt.sign(payload, SECRET_KEY, {
-//                 expiresIn: 60 * 60 * 24 // истекает через 24 часа
-//             });
-
-//             res.json({ success: true, message: 'Запрос выполнен. Токен получен', token: token });
-//             //res.status(200).json({ results });
-//         }
-//     } catch (e) {
-//         console.log(e);
-//         res.sendStatus(500);
-//     }
-// })
-
-// auth.get('/getuser', async (req, res) => {
-
-//     //Проверка токена из видео урока
-//     const authHeader = req.get('Authorization');
-
-//     if (!authHeader) {
-//         res.status(401).json({ success: false, message: "Token not provided!" });
-//     }
-
-//     const token = authHeader.replace('token ', '')
-
-//     //console.log('middleware ' + token);
-
-//     try {
-//         const decoded = jwt.verify(token, SECRET_KEY);
-
-//         //ошибка заголовков
-//         //res.status(200).json({ success: true, message: 'Good to authenticate token.' });
-
-//         //console.log('decode ' + decoded.role + ' ' + decoded.name);
-//         res.send(decoded);
-
-//     } catch (e) {
-//         if (e instanceof jwt.JsonWebTokenError) {
-//             res.status(401).json({ success: false, message: "Token invalid!" });
-//         }
-//     }
-// });
-
-//fmt.Println(t.Login)
-//fmt.Println(t.Password)
-
-//login := r.FormValue("login")
-//password := r.FormValue("password")
-//fmt.Println(password)
-
-//json.NewEncoder(w).Encode(account)
-
-// Глобальный секретный ключ
-//var mySigningKey = []byte("secret")
-
-// Создаем новый токен
-//token := jwt.New(jwt.SigningMethodHS256)
-
-// Устанавливаем набор параметров для токена
-//token.Claims["admin"] = true
-//token.Claims["name"] = "Ado Kukic"
-//token.Claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-
-// Подписываем токен нашим секретным ключем
-//tokenString, _ := token.SignedString(mySigningKey)
-
-// Отдаем токен клиенту
-//w.Write([]byte(tokenString))
-//json.NewEncoder(w).Encode(tokenString)
+//Генерация хэша на основе пароля введенного пользователем
+//HashPassword(password)

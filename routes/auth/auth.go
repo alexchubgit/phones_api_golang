7701523ee8f801bb927c2ece5eca0d1874b45ff4
Alex Chub <alex.chub.24@gmail.com>
2021-08-03 16:29:07 +0900

    work auth 03.08.2021

--- routes/auth/auth.go
+++ routes/auth/auth.go
@@ -319,7 +319,7 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 	// We ensure that a new token is not issued until enough time has elapsed
 	// In this case, a new token will only be issued if the old token is within
 	// 30 seconds of expiry. Otherwise, return a bad request status
-	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30*time.Second {
+	if time.Until(claims.StandardClaims.ExpiresAt) > 30*time.Second {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
